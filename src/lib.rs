9c0a52325306dcb0ccf53b7aef7b67822a53d664
Patrick H Madden <pmadden@acm.org>
2024-11-16 11:30:36 -0500

    Updated pstools to actually do something. But will move bbox and point here shortly

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,14 @@
+use std::fs::File;
+use std::io::Write;
+use std::path::Path;
+
 #[derive(Clone,Copy)]
 pub struct LBBox {
-    line: bool,
-    llx: f32,
-    lly: f32,
-    urx: f32,
-    ury: f32,
+    pub line: bool,
+    pub llx: f32,
+    pub lly: f32,
+    pub urx: f32,
+    pub ury: f32,
 }
 
 #[derive(Clone,Copy)]
@@ -33,20 +37,7 @@ pub struct Events {
     pub e: Vec<PSEvent>,
 }
 
-pub fn generate(events: &Events) {
-    for e in &events.e {
-        println!("Got event ");
-        unsafe {
-            if e.tag == PSTag::C {
-                let c = e.event.color;
-                println!("Color {} {} {} {}", c.r, c.g, c.b, c.a);
-            }
-            if e.tag == PSTag::B {
 
-            }
-        }
-    }
-}
 
 
 pub struct PSTool {
@@ -76,6 +67,100 @@ impl PSTool {
                     }
                     }
                 });
+    }
+    pub fn add_box(&mut self, llx: f32, lly: f32, urx: f32, ury: f32) {
+        self.e.e.push(
+            PSEvent {
+                tag: PSTag::B,
+                event: PSUnion {
+                    line: LBBox {
+                        line: false,
+                        llx: llx,
+                        lly: lly,
+                        urx: urx,
+                        ury: ury,
+                    }
+                }
+            }
+        );
+    }
+
+    pub fn bbox(&self) -> (f32, f32, f32, f32) {
+        // BBox started?
+        let mut mark = false;
+        let mut llx = 0.0;
+        let mut lly = 0.0;
+        let mut urx = 0.0;
+        let mut ury = 0.0;
+
+        for e in &self.e.e {
+            unsafe {
+                if e.tag == PSTag::C {
+
+                }
+                if e.tag == PSTag::B {
+                    if !mark {
+                        llx = e.event.line.llx.min(e.event.line.urx);
+                        lly = e.event.line.lly.min(e.event.line.lly);
+                        urx = e.event.line.llx.max(e.event.line.urx);
+                        ury = e.event.line.lly.max(e.event.line.ury);
+                        mark = true;
+                    } else {
+                        llx = llx.min(e.event.line.llx);
+                        llx = llx.min(e.event.line.urx);
+                        lly = lly.min(e.event.line.llx);
+                        lly = lly.min(e.event.line.urx);
+
+                        urx = urx.max(e.event.line.llx);
+                        urx = urx.max(e.event.line.urx);
+                        ury = ury.max(e.event.line.lly);
+                        ury = ury.max(e.event.line.ury);
+                    }
+                }
+            }
+        }
+
+        // Expand the bbox slightly
+        llx = llx - 10.0;
+        lly = lly - 10.0;
+        urx = urx + 10.0;
+        ury = ury + 10.0;
+
+        (llx, lly, urx, ury)
+    }
+
+    pub fn generate(&self, filepath: String) {
+        let mut f = File::create(filepath).unwrap();
+        let (origin_x, origin_y, urx, ury) = self.bbox();
+        println!("Bounding box {} {}  {} {}", origin_x, origin_y, urx, ury);
+
+        writeln!(&mut f, "%!PS-Adobe-3.0 EPSF-3.0").unwrap();
+        writeln!(&mut f, "%%DocumentData: Clean7Bit").unwrap();
+        writeln!(&mut f, "%%Origin: {} {}", origin_x, origin_y).unwrap();
+        writeln!(&mut f, "%%BoundingBox: {} {} {} {}", origin_x, origin_y, urx - origin_x, ury - origin_y).unwrap();
+        writeln!(&mut f, "%%LanguageLevel: 2").unwrap();
+        writeln!(&mut f, "%%Pages: 1").unwrap();
+        writeln!(&mut f, "%%Page: 1 1").unwrap();
+        writeln!(&mut f, "/Courier findfont 15 scalefont setfont").unwrap();
+        for e in &self.e.e {
+            // println!("Got event ");
+            unsafe {
+                if e.tag == PSTag::C {
+                    let c = e.event.color;
+                    writeln!(&mut f, "{} {} {} setrgbcolor", c.r, c.g, c.b).unwrap();
+                }
+                if e.tag == PSTag::B {
+                    writeln!(&mut f, "newpath {} {} moveto", e.event.line.llx, e.event.line.lly).unwrap();
+                    writeln!(&mut f, "{} {} lineto", e.event.line.llx, e.event.line.ury).unwrap();
+                    writeln!(&mut f, "{} {} lineto", e.event.line.urx, e.event.line.ury).unwrap();
+                    writeln!(&mut f, "{} {} lineto", e.event.line.urx, e.event.line.lly).unwrap();    
+                    writeln!(&mut f, "{} {} lineto", e.event.line.llx, e.event.line.lly).unwrap();
+                    writeln!(&mut f, "closepath fill").unwrap();
+                }
+            }
+        }
+        writeln!(&mut f, "%%EOF\n").unwrap();
+
     }
 }
 pub fn add(left: u64, right: u64) -> u64 {
--- src/main.rs
+++ src/main.rs
@@ -6,28 +6,13 @@ use pstools_r::{*};
 
 fn main() {
     println!("PSTools main!");
-
-    let mut e = pstools_r::Events {
-        e: Vec::new(),
-    };
-
-    e.e.push(PSEvent {
-        tag: PSTag::C,
-        event: PSUnion {
-            color:
-              Color {
-                 r: 3.3,
-                 g: 0.045,
-                 b: 0.2,
-                 a: 1.0,
-                fill: true,
-              }
-            }
-    });
-
-    pstools_r::generate(&e);
-
     let mut pst = PSTool::new();
-    pst.add_color(0.3, 0.4, 0.5, 0.2);
+
+    pst.add_color(0.3, 0.4, 0.2, 1.0);
+    pst.add_box(5.0, 5.0, 20.0, 30.0);
+    pst.add_color(0.8, 0.1, 0.2, 1.0);
+    pst.add_box(35.0, 19.0, 7.0, 16.0);
+    pst.generate("testfile.ps".to_string());
+
 
 }
